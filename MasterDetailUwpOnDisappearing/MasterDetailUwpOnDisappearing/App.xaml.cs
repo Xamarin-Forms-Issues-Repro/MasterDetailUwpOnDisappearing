51157a7ac78d7b2242addfa6f3ba13483bd8e1f0
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-04-22 14:53:28 +0200

    init

---
+++ MasterDetailUwpOnDisappearing/MasterDetailUwpOnDisappearing/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MasterDetailUwpOnDisappearing.Services;
+using MasterDetailUwpOnDisappearing.Views;
+
+namespace MasterDetailUwpOnDisappearing
+{
+	public partial class App : Application
+	{
+
+		public MainPage MasterDetailPage { get; set; }
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new NavigationPage(new StartPage());
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ MasterDetailUwpOnDisappearing/MasterDetailUwpOnDisappearing/StartPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailUwpOnDisappearing
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+		public StartPage()
+		{
+			InitializeComponent();
+		
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			var masterDetail = new Views.MainPage();
+			if (Application.Current is App app)
+				app.MasterDetailPage = masterDetail;
+			
+			Navigation.PushAsync(masterDetail);
+		}
+	}
+}
